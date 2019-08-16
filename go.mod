6b32333b3a279cdf55711470afbb57891ca164e0
Eduard Urbach <e.urbach@gmail.com>
2019-08-16 20:44:52 +0900

    Use akyoto/assert for tests

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/akyoto/stringutils
 
 go 1.12
 
-require github.com/frankban/quicktest v1.4.0
+require github.com/akyoto/assert v0.1.2
--- unsafe/Unsafe_test.go
+++ unsafe/Unsafe_test.go
@@ -3,20 +3,18 @@ package unsafe_test
 import (
 	"testing"
 
+	"github.com/akyoto/assert"
 	"github.com/akyoto/stringutils/unsafe"
-	qt "github.com/frankban/quicktest"
 )
 
 func TestBytesToString(t *testing.T) {
 	out := "hello"
 	in := []byte(out)
-	c := qt.New(t)
-	c.Assert(out, qt.DeepEquals, unsafe.BytesToString(in))
+	assert.DeepEqual(t, out, unsafe.BytesToString(in))
 }
 
 func TestStringToBytes(t *testing.T) {
 	in := "hello"
 	out := []byte(in)
-	c := qt.New(t)
-	c.Assert(out, qt.DeepEquals, unsafe.StringToBytes(in))
+	assert.DeepEqual(t, out, unsafe.StringToBytes(in))
 }
